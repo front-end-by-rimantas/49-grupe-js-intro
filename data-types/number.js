77bf57110b70fa79dbabf38e544688f536293d8a
Rimantas <belauzas@gmail.com>
2024-01-29 14:06:01 +0200

    Initial commit

---
+++ data-types/number.js
@@ -0,0 +1,20 @@
+/*
+NUMBERS
+
+- normalus
+    - teigiami/neigiami
+    - sveikieji/desimtainiai
+- ne normalus
+    - Infinity (begalybes)
+    - NaN (not-a-number)
+
+*/
+
+console.log(1);
+console.log(2);
+console.log(3.1415);
+console.log(-4);
+console.log(-5.555);
+console.log(Infinity);
+console.log(-Infinity);
+console.log(NaN);

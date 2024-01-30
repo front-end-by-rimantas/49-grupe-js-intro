c4055dc96f97de61a09466b4615df6b78185b94b
Rimantas <belauzas@gmail.com>
2024-01-30 12:46:30 +0200

    Math

--- data-types/number.js
+++ data-types/number.js
@@ -8,6 +8,11 @@ NUMBERS
     - Infinity (begalybes)
     - NaN (not-a-number)
 
+OPERATORIAI:
+- priskyrimo (=)
+- matematiniai:
+    - aritmetiniai: +, -, /, *, % (liekana), ** (kelimas laipsniu)
+
 */
 
 console.log(1);
@@ -18,3 +23,71 @@ console.log(-5.555);
 console.log(Infinity);
 console.log(-Infinity);
 console.log(NaN);
+
+console.clear();
+
+console.log(555);
+
+const a = 1;
+const b = -22;
+const PI = 3.1415;
+const pi = 777;
+const duZodziai = 8888;
+const trysIlgiZodziai = 8888;
+
+console.log(a);
+console.log(b);
+console.log(PI);
+console.log(pi);
+
+const number1 = 111;
+const number2 = 222;
+const number3 = 333;
+
+console.log(number1);
+console.log(number2);
+console.log(number3);
+
+console.clear();
+
+const sum12 = number1 + number2;
+console.log(sum12);
+
+const sum23 = number2 + number3;
+console.log(sum23);
+
+const sum13 = number1 + number3;
+console.log(sum13);
+
+const x = 1 + 2 + 3 + sum12;
+console.log(x);
+
+console.clear();
+
+const n1 = 7;
+const n2 = 5;
+
+const sum = n1 + n2;
+console.log(n1, '+', n2, '=', sum);
+
+const diff = n1 - n2;
+console.log(n1, '-', n2, '=', diff);
+
+const multi = n1 * n2;
+console.log(n1, '*', n2, '=', multi);
+
+const div = n1 / n2;
+console.log(n1, '/', n2, '=', div);
+
+const du0 = 2 ** 0;
+const du1 = 2 ** 1;
+const du2 = 2 ** 2;
+const du3 = 2 ** 3;
+const du4 = 2 ** 4;
+
+console.log(du0);
+console.log(du1);
+console.log(du2, 2 * 2);
+console.log(du3, 2 * 2 * 2);
+console.log(du4, 2 * 2 * 2 * 2);
+

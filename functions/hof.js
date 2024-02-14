e3a97f419a17318394754b840f807acd57e78b3d
Rimantas <belauzas@gmail.com>
2024-02-14 13:44:37 +0200

    HOF, tik jau tikras

--- functions/hof.js
+++ functions/hof.js
@@ -22,3 +22,32 @@ console.log(res1);
 console.log(res2);
 console.log(res3);
 console.log(res4);
+
+console.clear();
+
+function firstLetter(text) {
+    return text[0];
+}
+
+function lastLetter(text) {
+    return text[text.length - 1];
+    // return text.at(-1);
+}
+
+function giveMeLetter(text, func) {
+    return func(text);
+}
+
+console.log(firstLetter('Chuck'));
+console.log(lastLetter('Chuck'));
+
+console.log(giveMeLetter('Chuck', firstLetter));
+console.log(giveMeLetter('Norris', lastLetter));
+
+const sum = (a, b) => a + b;
+const diff = (a, b) => a - b;
+
+const calc = (a, func, b) => func(a, b);
+
+console.log(calc(7, sum, 5));
+console.log(calc(7, diff, 5));

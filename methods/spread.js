c3db912375c98c9061157bf9fb76410c0a2c4f5e
Rimantas <belauzas@gmail.com>
2024-02-19 13:54:49 +0200

    Object spread

--- methods/spread.js
+++ methods/spread.js
@@ -31,3 +31,62 @@ console.log(rest12);
 const rest123 = [...t1, ...t2, ...t3];
 console.log(rest123);
 
+console.clear();
+
+const car = {
+    brand: 'Audi',
+    model: 80,
+    color: 'red',
+    doors: 5,
+    wheels: 4,
+    gear: {
+        type: 'manual',
+        count: 5,
+    },
+};
+
+const { brand, color, gear, ...restCar } = car;
+const { type } = gear;
+
+console.log(brand, color, gear, type);
+console.log(restCar);
+
+
+const xBrand = car.brand;
+const xColor = car.color;
+const xGear = car.gear;
+const xType = xGear.type;
+
+const usedCarKeys = ['brand', 'color', 'gear'];
+const xRestCar = {};
+//- brand, color, gear
+//+ model, doors, wheels
+
+for (const key in car) {
+    if (!usedCarKeys.includes(key)) {
+        xRestCar[key] = car[key];
+    }
+}
+
+console.log(xRestCar);
+
+const o1 = { a: 'aaa' };
+const o2 = { b: 'bbb' };
+const o3 = { c: 'ccc' };
+
+const o12 = {};
+
+for (const key in o1) {
+    o12[key] = o1[key];
+}
+
+const o2keys = Object.keys(o2);
+for (let i = 0; i < o2keys.length; i++) {
+    const key = o2keys[i];
+    o12[key] = o2[key];
+}
+
+console.log(o12);
+
+const restO12 = { ...o1, ...o2 };
+console.log(restO12);

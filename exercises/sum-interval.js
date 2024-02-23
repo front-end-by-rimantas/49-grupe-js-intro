aae05cb6b47e295a6b7f45d2a20a26eb9672f317
Rimantas <belauzas@gmail.com>
2024-02-23 10:54:19 +0200

    Solved

--- exercises/sum-interval.js
+++ exercises/sum-interval.js
@@ -1,27 +1,50 @@
-function sumInterval(start, end) {
-    if (start < 0 && end <= 0) {
-        const a = (start * (end - 1) / 2);
-        const b = (end * (0 - 1) / 2);
-        return a + b;
-    }
+function integral(n) {
+    n = Math.abs(n);
+    return n * (n + 1) / 2;
+}
 
-    if (start <= 0 && end >= 0) {
+function sumInterval(start, end) {
+    if (-start === end) {
         return 0;
     }
 
-    if (start >= 0 && end > 0) {
-        return 1;
+    if (start < 0 && end < 0) {
+        return integral(end + 1) - integral(start);
+    }
+
+    if (start < 0 && end === 0) {
+        return -integral(start);
+    }
+
+    if (start < 0 && end > 0) {
+        return integral(end) - integral(start);
+    }
+
+    if (start === 0 && end > 0) {
+        return integral(end);
+    }
+
+    if (start > 0 && end > 0) {
+        return integral(end) - integral(start - 1);
     }
 }
 
+console.log(sumInterval(-4, -4), '-->', -4);
+console.log(sumInterval(-4, -3), '-->', -7);
+console.log(sumInterval(-4, -2), '-->', -9);
 console.log(sumInterval(-4, -1), '-->', -10);
+console.log(sumInterval(-4, 0), '-->', -10);
+console.log(sumInterval(-3, 0), '-->', -6);
+console.log(sumInterval(-3, 1), '-->', -5);
+console.log(sumInterval(-3, 3), '-->', 0);
+console.log(sumInterval(-1, 3), '-->', 5);
+console.log(sumInterval(-1, 4), '-->', 9);
+console.log(sumInterval(-2, 3), '-->', 3);
+console.log(sumInterval(-4, 3), '-->', -4);
+console.log(sumInterval(0, 0), '-->', 0);
+console.log(sumInterval(0, 2), '-->', 3);
+console.log(sumInterval(0, 3), '-->', 6);
 console.log(sumInterval(0, 4), '-->', 10);
 console.log(sumInterval(1, 4), '-->', 10);
 console.log(sumInterval(2, 4), '-->', 9);
 console.log(sumInterval(3, 4), '-->', 7);
-console.log(sumInterval(-1, 4), '-->', 9);
-console.log(sumInterval(-2, 3), '-->', 3);
-console.log(sumInterval(-4, 3), '-->', -4);
-console.log(sumInterval(-4, 0), '-->', -10);
-console.log(sumInterval(0, 0), '-->', 0);
-console.log(sumInterval(-3, 3), '-->', 0);
